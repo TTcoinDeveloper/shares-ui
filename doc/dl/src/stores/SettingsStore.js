b1d1288d429b1d843b1f7d3c2245790d824ddc48
TTcoinDeveloper <adamashton11@gmail.com>
2018-12-27 09:26:03 -0400

    new

---
+++ doc/dl/src/stores/SettingsStore.js
@@ -0,0 +1,198 @@
+var alt = require("../alt-instance");
+var SettingsActions = require("../actions/SettingsActions");
+
+var Immutable = require("immutable");
+var _ =require("lodash");
+
+const STORAGE_KEY = "__graphene__";
+const CORE_ASSET = "BTS"; // Setting this to BTS to prevent loading issues when used with BTS chain which is the most usual case currently
+
+var ls = typeof localStorage === "undefined" ? null : localStorage;
+
+class SettingsStore {
+    constructor() {
+        this.exportPublicMethods({getSetting: this.getSetting.bind(this)});
+
+        this.settings = Immutable.Map({
+            locale: "en",
+            connection: "wss://bitshares.openledger.info/ws",
+            faucet_address: "https://bitshares.openledger.info",
+            unit: CORE_ASSET,
+            showSettles: false
+        });
+
+        this.viewSettings =  Immutable.Map({
+            cardView: true
+        });
+
+        this.starredMarkets = Immutable.Map([
+            [CORE_ASSET + "_BTC", {"quote": CORE_ASSET,"base": "BTC"}],
+            [CORE_ASSET + "_CNY", {"quote": CORE_ASSET,"base": "CNY"}],
+            [CORE_ASSET + "_EUR", {"quote": CORE_ASSET,"base": "EUR"}],
+            [CORE_ASSET + "_GOLD", {"quote": CORE_ASSET,"base": "GOLD"}],
+            [CORE_ASSET + "_SILVER", {"quote": CORE_ASSET,"base": "SILVER"}],
+            [CORE_ASSET + "_USD", {"quote": CORE_ASSET,"base": "USD"}],
+            ["BTC_USD", {"quote":"BTC","base":"USD"}],
+            ["BTC_CNY", {"quote":"BTC","base":"CNY"}],
+            [CORE_ASSET + "_OPENBTC", {"quote": CORE_ASSET,"base": "OPENBTC"} ],
+            [CORE_ASSET + "_OPENMUSE", {"quote": CORE_ASSET,"base": "OPENMUSE"} ],
+            [CORE_ASSET + "_TRADE.BTC", {"quote": CORE_ASSET,"base": "TRADE.BTC"} ],
+            ["TRADE.BTC_BTC", {"quote":"TRADE.BTC","base": "BTC"} ],
+            [CORE_ASSET + "_METAFEES", {"quote": CORE_ASSET,"base": "METAFEES"} ],
+            [CORE_ASSET + "_OBITS", {"quote": CORE_ASSET,"base": "OBITS"} ],
+            [CORE_ASSET + "_TRADE.MUSE", {"quote": CORE_ASSET,"base": "TRADE.MUSE"} ],
+            ["METAEX.BTC_BTC", {"quote":"METAEX.BTC","base": "BTC"} ],
+            [CORE_ASSET + "_METAEX.BTC", {"quote": CORE_ASSET,"base": "METAEX.BTC" } ]
+        ]);
+
+        this.starredAccounts = Immutable.Map();
+
+        // If you want a default value to be translated, add the translation to settings in locale-xx.js
+        // and use an object {translate: key} in the defaults array
+        this.defaults = {
+            locale: [
+                "en",
+                "cn",
+                "fr",
+                "ko",
+                "de",
+                "es",
+                "tr"
+            ],
+            connection: [
+                "wss://bitshares.openledger.info/ws"
+            ],
+            unit: [
+                CORE_ASSET,
+                "USD",
+                "CNY",
+                "BTC",
+                "EUR"
+            ],
+            showSettles: [
+                {translate: "yes"},
+                {translate: "no"}
+            ]
+            // confirmMarketOrder: [
+            //     {translate: "confirm_yes"},
+            //     {translate: "confirm_no"}
+            // ]
+        };
+
+        this.bindListeners({
+            onChangeSetting: SettingsActions.changeSetting,
+            onChangeViewSetting: SettingsActions.changeViewSetting,
+            onAddStarMarket: SettingsActions.addStarMarket,
+            onRemoveStarMarket: SettingsActions.removeStarMarket,
+            onAddStarAccount: SettingsActions.addStarAccount,
+            onRemoveStarAccount: SettingsActions.removeStarAccount,
+            onAddWS: SettingsActions.addWS,
+            onRemoveWS: SettingsActions.removeWS
+        });
+
+        if (this._lsGet("settings_v3")) {
+            this.settings = Immutable.Map(_.merge(this.settings.toJS(), JSON.parse(this._lsGet("settings_v3"))));
+        }
+
+        if (this._lsGet("starredMarkets")) {
+            this.starredMarkets = Immutable.Map(JSON.parse(this._lsGet("starredMarkets")));
+        }
+
+        if (this._lsGet("starredAccounts")) {
+            this.starredAccounts = Immutable.Map(JSON.parse(this._lsGet("starredAccounts")));
+        }
+
+        if (this._lsGet("defaults_v1")) {
+            this.defaults = _.merge(this.defaults, JSON.parse(this._lsGet("defaults_v1")));
+        }
+
+        if (this._lsGet("viewSettings_v1")) {
+            this.viewSettings = Immutable.Map(JSON.parse(this._lsGet("viewSettings_v1")));
+        }
+    }
+
+    getSetting(setting) {
+        return this.settings.get(setting);
+    }
+
+    onChangeSetting(payload) {
+        this.settings = this.settings.set(
+            payload.setting,
+            payload.value
+        );
+
+        this._lsSet("settings_v3", this.settings.toJS());
+    }
+
+    onChangeViewSetting(payload) {
+        for (key in payload) {
+            this.viewSettings = this.viewSettings.set(key, payload[key]);
+        }
+
+        this._lsSet("viewSettings_v1", this.viewSettings.toJS());
+    }
+
+    _lsGet(key) {
+        if (ls) {
+            return ls.getItem(STORAGE_KEY + key);
+        }
+    }
+
+    _lsSet(key, object) {
+        if (ls) {
+            ls.setItem(STORAGE_KEY + key, JSON.stringify(object));
+        }
+
+    }
+
+    onAddStarMarket(market) {
+        let marketID = market.quote + "_" + market.base;
+
+        if (!this.starredMarkets.has(marketID)) {
+            this.starredMarkets = this.starredMarkets.set(marketID, {quote: market.quote, base: market.base});
+
+            this._lsSet("starredMarkets", this.starredMarkets.toJS());
+        } else {
+            return false;
+        }
+    }
+
+    onRemoveStarMarket(market) {
+        let marketID = market.quote + "_" + market.base;
+
+        this.starredMarkets = this.starredMarkets.delete(marketID);
+
+        this._lsSet("starredMarkets", this.starredMarkets.toJS());
+    }
+
+    onAddStarAccount(account) {
+        if (!this.starredAccounts.has(account)) {
+            this.starredAccounts = this.starredAccounts.set(account, {name: account});
+
+            this._lsSet("starredAccounts", this.starredAccounts.toJS());
+        } else {
+            return false;
+        }
+    }
+
+    onRemoveStarAccount(account) {
+
+        this.starredAccounts = this.starredAccounts.delete(account);
+
+        this._lsSet("starredAccounts", this.starredAccounts.toJS());
+    }
+
+    onAddWS(ws) {
+        this.defaults.connection.push(ws);
+        this._lsSet("defaults_v1", this.defaults);
+    }
+
+    onRemoveWS(index) {
+        if (index !== 0) { // Prevent removing the default connection
+            this.defaults.connection.splice(index, 1);
+            this._lsSet("defaults_v1", this.defaults);
+        }
+    }
+}
+
+module.exports = alt.createStore(SettingsStore, "SettingsStore");
---
+++ doc/web/app/components/Account/AccountAssetCreate.jsx
@@ -0,0 +1,358 @@
+import React from "react";
+import Translate from "react-translate-component";
+import classnames from "classnames";
+import validation from "common/validation";
+import AssetActions from "actions/AssetActions";
+import HelpContent from "../Utility/HelpContent";
+import utils from "common/utils";
+import ChainStore from "api/ChainStore";
+import FormattedAsset from "../Utility/FormattedAsset";
+import counterpart from "counterpart";
+import ChainTypes from "../Utility/ChainTypes";
+import BindToChainState from "../Utility/BindToChainState";
+import FormattedPrice from "../Utility/FormattedPrice";
+import AccountSelector from "../Account/AccountSelector";
+import AssetSelector from "../Utility/AssetSelector";
+import LinkToAccountById from "../Blockchain/LinkToAccountById";
+import AccountInfo from "./AccountInfo";
+import big from "bignumber.js";
+import cnames from "classnames";
+import assetUtils from "common/asset_utils";
+import Tabs, {Tab} from "../Utility/Tabs";
+
+let MAX_SAFE_INT = new big("9007199254740991");
+
+@BindToChainState()
+class AccountAssetCreate extends React.Component {
+
+    static propTypes = {
+        core: ChainTypes.ChainAsset.isRequired,
+        globalObject: ChainTypes.ChainObject.isRequired
+    };
+
+    static defaultProps = {
+        globalObject: "2.0.0",
+        core: "1.3.0"
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.state = this.resetState(props);
+    }
+
+    resetState(props) {
+        // let asset = props.asset.toJS();
+        let isBitAsset = false;
+        let precision = utils.get_asset_precision(4);
+        let corePrecision = utils.get_asset_precision(props.core.get("precision"));
+
+        let flagBooleans = assetUtils.getFlagBooleans(0);
+        let permissionBooleans = assetUtils.getFlagBooleans(79);
+        let flags = assetUtils.getFlags(flagBooleans);
+        let permissions = assetUtils.getPermissions(permissionBooleans);
+
+        return {
+
+            update: {
+                symbol: "",
+                precision: 4,
+                max_supply: 0,
+                max_market_fee: 0,
+                market_fee_percent: 0,
+                description: ""
+            },
+            errors: {
+                max_supply: null
+            },
+            isValid: true,
+            flagBooleans: flagBooleans,
+            permissionBooleans: permissionBooleans,
+            isBitAsset: isBitAsset
+        };
+    }
+
+
+
+    _createAsset(e) {
+        e.preventDefault();
+        let {update, flagBooleans, permissionBooleans} = this.state;
+        let {account} = this.props;
+
+        let flags = assetUtils.getFlags(flagBooleans);
+        let permissions = assetUtils.getPermissions(permissionBooleans);
+
+        AssetActions.createAsset(account.get("id"), update, flags, permissions).then(result => {
+            console.log("... AssetActions.updateAsset(account_id, update)", account.get("id"),  update, flags, permissions)
+        });
+    }
+
+    _hasChanged() {
+        return !utils.are_equal_shallow(this.state, this.resetState(this.props));
+    }
+
+    _reset(e) {
+        e.preventDefault();
+
+        this.setState(
+            this.resetState(this.props)
+        );
+    }
+
+    _forcePositive(number) {
+        return parseFloat(number) < 0 ? "0" : number;
+    }
+
+    _onUpdateInput(value, e) {
+        let {update} = this.state;
+        let updateState = true;
+        let precision = utils.get_asset_precision(this.state.update.precision);
+
+        switch (value) {
+            case "market_fee_percent":
+                update[value] = this._forcePositive(e.target.value);
+                break;
+
+            case "max_market_fee":
+                if ((new big(e.target.value)).times(precision).gt(MAX_SAFE_INT)) {
+                    return this.setState({errors: {max_market_fee: "The number you tried to enter is too large"}});
+                }
+                e.target.value = utils.limitByPrecision(e.target.value, this.state.update.precision);
+                update[value] = e.target.value;
+                break;
+
+            case "precision":
+                // Enforce positive number
+                update[value] = this._forcePositive(e.target.value);
+                break;
+
+            case "max_supply":
+                if ((new big(e.target.value)).times(precision).gt(MAX_SAFE_INT)) {
+                    return this.setState({errors: {max_supply: "The number you tried to enter is too large"}});
+                }
+                e.target.value = utils.limitByPrecision(e.target.value, this.state.update.precision);
+                update[value] = e.target.value;
+                break;
+
+            case "symbol":
+                // Enforce uppercase
+                e.target.value = e.target.value.toUpperCase();
+                // Enforce characters
+                let regexp = new RegExp("^[\.A-Z]+$");
+                if (e.target.value !== "" && !regexp.test(e.target.value)) {
+                    break;
+                }
+                ChainStore.getAsset(e.target.value);
+                update[value] = this._forcePositive(e.target.value);
+                break;
+
+            default:
+                update[value] = e.target.value;
+                break;
+        }
+
+        if (updateState) {
+            this.setState({update: update});
+            this._validateEditFields(update);
+        }
+    }
+
+    _validateEditFields( new_state ) {
+        let {core} = this.props;
+
+        let errors = {
+            max_supply: null
+        };
+
+        errors.symbol = validation.is_valid_symbol_error(new_state.symbol);
+        let existingAsset = ChainStore.getAsset(new_state.symbol);
+        if (existingAsset) {
+            errors.symbol = counterpart.translate("account.user_issued_assets.exists");
+        }
+
+        errors.max_supply = new_state.max_supply <= 0 ? counterpart.translate("account.user_issued_assets.max_positive") : null;
+
+        let isValid = !errors.symbol && !errors.max_supply;
+
+        this.setState({isValid: isValid, errors: errors});
+    }
+
+    _onFlagChange(key) {
+        let booleans = this.state.flagBooleans;
+        booleans[key] = !booleans[key];
+        this.setState({
+            flagBooleans: booleans
+        });
+    }
+
+    _onPermissionChange(key) {
+        let booleans = this.state.permissionBooleans;
+        booleans[key] = !booleans[key];
+        this.setState({
+            permissionBooleans: booleans
+        });
+    }
+
+    render() {
+        let {account, account_name, globalObject, core} = this.props;
+        let {errors, isValid, update, assets, flagBooleans, permissionBooleans} = this.state;
+
+        // Estimate the asset creation fee from the symbol character length
+        let symbolLength = update.symbol.length, createFee = "N/A";
+
+        if(symbolLength === 3) {
+            createFee = <FormattedAsset amount={utils.estimateFee("asset_create", ["symbol3"], globalObject)} asset={"1.3.0"} />;
+        }
+        else if(symbolLength === 4) {
+            createFee = <FormattedAsset amount={utils.estimateFee("asset_create", ["symbol4"], globalObject)} asset={"1.3.0"} />;
+        }
+        else if(symbolLength > 4) {
+            createFee = <FormattedAsset amount={utils.estimateFee("asset_create", ["long_symbol"], globalObject)} asset={"1.3.0"} />;
+        }
+        // let cr_quote_asset = ChainStore.getAsset(core_exchange_rate.quote.asset_id);
+        // let precision = utils.get_asset_precision(cr_quote_asset.get("precision"));
+        // let cr_base_asset = ChainStore.getAsset(core_exchange_rate.base.asset_id);
+        // let basePrecision = utils.get_asset_precision(cr_base_asset.get("precision"));
+
+        // let cr_quote_amount = (new big(core_exchange_rate.quote.amount)).times(precision).toString();
+        // let cr_base_amount = (new big(core_exchange_rate.base.amount)).times(basePrecision).toString();
+
+        // console.log("flags:", assetUtils.getFlags(flagBooleans), "permissions:", assetUtils.getPermissions(permissionBooleans));
+
+        // Loop over flags        
+        let flags = [];
+        for (let key in permissionBooleans) {
+            if (permissionBooleans[key] && key !== "charge_market_fee") {
+                flags.push(
+                    <table key={"table_" + key} className="table">
+                        <tbody>
+                            <tr>
+                                <td style={{border: "none", width: "80%"}}><Translate content={`account.user_issued_assets.${key}`} />:</td>
+                                <td style={{border: "none"}}>
+                                    <div className="switch" style={{marginBottom: "10px"}} onClick={this._onFlagChange.bind(this, key)}>
+                                        <input type="checkbox" checked={flagBooleans[key]} />
+                                        <label />
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                )
+            }
+        }
+
+        // Loop over permissions
+        let permissions = [];
+        for (let key in permissionBooleans) {
+                permissions.push(
+                    <table key={"table_" + key} className="table">
+                        <tbody>
+                            <tr>
+                                <td style={{border: "none", width: "80%"}}><Translate content={`account.user_issued_assets.${key}`} />:</td>
+                                <td style={{border: "none"}}>
+                                    <div className="switch" style={{marginBottom: "10px"}} onClick={this._onPermissionChange.bind(this, key)}>
+                                        <input type="checkbox" checked={permissionBooleans[key]} onChange={() => {}}/>
+                                        <label />
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                )
+        }
+
+        return (
+            <div className="grid-block">
+                <div className="grid-content">
+                    <h3><Translate content="header.create_asset" /></h3>
+                    <Tabs setting="createAssetTab" style={{maxWidth: "800px"}} contentClass="grid-block shrink small-vertical medium-horizontal">
+
+                        <Tab title="account.user_issued_assets.primary">
+                            <div className="small-12 large-6 grid-content">
+                                <h3><Translate content="account.user_issued_assets.primary" /></h3>
+                                <label><Translate content="account.user_issued_assets.symbol" />
+                                    <input type="text" value={update.symbol} onChange={this._onUpdateInput.bind(this, "symbol")}/>
+                                </label>
+                                { errors.symbol ? <p className="grid-content has-error">{errors.symbol}</p> : null}
+
+
+                                <label><Translate content="account.user_issued_assets.max_supply" /> {update.symbol ? <span>({update.symbol})</span> : null}
+                                    <input type="number" value={update.max_supply} onChange={this._onUpdateInput.bind(this, "max_supply")} />
+                                </label>
+                                { errors.max_supply ? <p className="grid-content has-error">{errors.max_supply}</p> : null}
+
+                                <label><Translate content="account.user_issued_assets.precision" />
+                                    <input type="number" value={update.precision} onChange={this._onUpdateInput.bind(this, "precision")} />
+                                </label>                                
+
+                                <Translate component="h3" content="account.user_issued_assets.description" />
+                                <label>
+                                    <textarea style={{height: "7rem"}} rows="1" value={update.description} onChange={this._onUpdateInput.bind(this, "description")} />
+                                </label>
+                            </div>
+                        </Tab>
+
+                        <Tab title="account.user_issued_assets.flags">
+                            <div className="small-12 large-6 grid-content">
+                                {permissionBooleans["charge_market_fee"] ? (
+                                    <div>
+                                        <Translate component="h3" content="account.user_issued_assets.market_fee" />
+                                        <table className="table">
+                                            <tbody>
+                                                <tr>
+                                                    <td style={{border: "none", width: "80%"}}><Translate content="account.user_issued_assets.charge_market_fee" />:</td>
+                                                    <td style={{border: "none"}}>
+                                                        <div className="switch" style={{marginBottom: "10px"}} onClick={this._onFlagChange.bind(this, "charge_market_fee")}>
+                                                            <input type="checkbox" checked={flagBooleans.charge_market_fee} />
+                                                            <label />
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                        <div className={cnames({disabled: !flagBooleans.charge_market_fee})}>
+                                        <label><Translate content="account.user_issued_assets.market_fee" /> (%)
+                                            <input type="number" value={update.market_fee_percent} onChange={this._onUpdateInput.bind(this, "market_fee_percent")}/>
+                                        </label>
+
+                                        <label>
+                                             <input type="number" value={update.max_market_fee} onChange={this._onUpdateInput.bind(this, "max_market_fee")}/>
+                                        </label>
+                                        { errors.max_market_fee ? <p className="grid-content has-error">{errors.max_market_fee}</p> : null}
+                                        </div>
+                                    </div>) : null}
+
+                                <h3><Translate content="account.user_issued_assets.flags" /></h3>
+                                {flags}
+                            </div>
+                        </Tab>
+
+                        <Tab title="account.permissions">
+                            <div className="small-12 large-6 grid-content">
+                                <p className="grid-content has-error"><Translate content="account.user_issued_assets.perm_warning" /></p>
+                                {permissions}
+                            </div>
+                        </Tab>
+                    </Tabs>
+
+                    <hr/>
+                    <div style={{paddingTop: "0.5rem"}}>
+                        <button className={classnames("button", {disabled: !isValid})} onClick={this._createAsset.bind(this)}>
+                            <Translate content="header.create_asset" />
+                        </button>
+                        <button className="button outline" onClick={this._reset.bind(this)} value={counterpart.translate("account.perm.reset")}>
+                            <Translate content="account.perm.reset" />
+                        </button>
+                        <br/>
+                        <br/>
+                        <p><Translate content="account.user_issued_assets.approx_fee" />: {createFee}</p>
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default AccountAssetCreate;
