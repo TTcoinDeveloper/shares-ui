6a13f215d8c53b86b7be758f2c7e49a360d5d47b
Cindy* <adamashton11@gmail.com>
2019-01-02 12:18:47 -0400

    Update webpack.config.js

--- web/conf/webpack.config.js
+++ web/conf/webpack.config.js
@@ -56,7 +56,7 @@ module.exports = function(options) {
             app: options.prod ?
                 path.resolve(root_dir, "app/Main.js") :
                 [
-                    "webpack-dev-server/client?http://localhost:8080",
+                    "webpack-dev-server/client?http://0.0.0.0:8080",
                     "webpack/hot/only-dev-server",
                     path.resolve(root_dir, "app/Main-dev.js")
                 ]
